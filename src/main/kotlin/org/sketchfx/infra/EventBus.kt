c5e3bd4abd204131c404bbb4dffb6c4c9e9e36df
Eugene Ryzhikov <eryzhikov@gmail.com>
2023-02-01 18:42:28 -0500

    Improved event bus

--- src/main/kotlin/org/sketchfx/infra/EventBus.kt
+++ src/main/kotlin/org/sketchfx/infra/EventBus.kt
@@ -4,24 +4,24 @@ interface Event
 
 class EventBus {
 
-    val allSubscribers = mutableMapOf<Class<out Event>, Set<(Event) -> Unit>>()
+    val allSubscribers = mutableMapOf<Class<out Event>, MutableSet<(Event) -> Unit>>()
 
-    inline fun <reified T: Event> subscribersFor(): Set<(T) -> Unit> {
-        return allSubscribers[T::class.java] ?: emptySet()
+    inline fun <reified T: Event> subscribers(): MutableSet<(T) -> Unit> {
+        @Suppress("UNCHECKED_CAST")
+        return allSubscribers.getOrPut( T::class.java) { mutableSetOf() } as MutableSet<(T) -> Unit>
     }
 
     inline fun <reified T:  Event> subscribe(noinline subscriber: (T) -> Unit) {
-        @Suppress("UNCHECKED_CAST")
-        allSubscribers[T::class.java] = (subscribersFor<T>() + subscriber) as Set<(Event) -> Unit>
+        subscribers<T>().add(subscriber)
     }
 
     inline fun <reified T: Event> unsubscribe(noinline subscriber: (T) -> Unit) {
-        @Suppress("UNCHECKED_CAST")
-        allSubscribers[T::class.java] = (subscribersFor<T>() + subscriber) as Set<(Event) -> Unit>
+        subscribers<T>().remove(subscriber)
     }
 
     inline fun <reified T: Event> publish(event: T) {
-        subscribersFor<T>().forEach { it(event) }
+        val subscribersForEvent = allSubscribers[T::class.java] ?: emptySet()
+        subscribersForEvent.forEach { it(event) }
     }
 }
 
