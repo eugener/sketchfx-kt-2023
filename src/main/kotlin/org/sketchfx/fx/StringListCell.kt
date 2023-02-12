25522e29b9b13d4e6987324bf5049b62485feb0e
Eugene Ryzhikov <eryzhikov@gmail.com>
2023-02-12 09:23:44 -0500

    Prevent horizontal selection for ListView

--- src/main/kotlin/org/sketchfx/fx/StringListCell.kt
+++ src/main/kotlin/org/sketchfx/fx/StringListCell.kt
@@ -6,6 +6,8 @@ class StringListCell<T>(className: String): ListCell<T>() {
 
     init {
         styleClass.addAll(className)
+        minWidth = 0.0;
+        prefWidth = 1.0;
     }
 
     override fun updateItem(item: T?, empty: Boolean) {
