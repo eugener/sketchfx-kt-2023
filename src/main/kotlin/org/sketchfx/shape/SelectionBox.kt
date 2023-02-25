a0f509267cb88f3c30a0507e7c1a364a97d5a65d
Eugene Ryzhikov <eryzhikov@gmail.com>
2023-02-25 13:01:16 -0500

    Improve selection box

--- src/main/kotlin/org/sketchfx/shape/SelectionBox.kt
+++ src/main/kotlin/org/sketchfx/shape/SelectionBox.kt
@@ -9,48 +9,51 @@ import javafx.scene.shape.Rectangle
 import org.sketchfx.canvas.CanvasViewModel
 import org.sketchfx.fx.BoundsExt.union
 
-class SelectionBox(selection: Collection<Shape>, private val context: CanvasViewModel): Group() {
+class SelectionBox(selection: Collection<Shape>, context: CanvasViewModel): Group() {
+
+
+
+    init {
+
+        val (selectionShapes, selectionBounds) =
+            selection.fold(Pair(listOf<Node>(), null as Bounds?)) { (selShapes, selBounds), shape ->
+                val shapeBounds = shape.boundsInParent
+                val b = selBounds?.let { selBounds.union(shapeBounds) } ?: shapeBounds
+                Pair(selShapes + Shape.selection(shape), b)
+            }
+
+        children.setAll((selectionShapes + Shape.selectionBounds(selectionBounds!!, context)))
+
+        children.addAll(
+            SelectionHandle(selectionBounds.minX, selectionBounds.minY, Cursor.NW_RESIZE, context),
+            SelectionHandle(selectionBounds.maxX, selectionBounds.maxY, Cursor.SE_RESIZE, context),
+            SelectionHandle(selectionBounds.maxX, selectionBounds.minY, Cursor.NE_RESIZE, context),
+            SelectionHandle(selectionBounds.minX, selectionBounds.maxY, Cursor.SW_RESIZE, context),
+        )
+    }
+
+}
+
+class SelectionHandle( x: Double, y: Double, cursor: Cursor, context: CanvasViewModel ): Group() {
 
     companion object {
         private const val handleSize: Int = 8
     }
 
-    init {
-
-        val (selectionShapes, selectionBounds) =
-            selection.fold( Pair(listOf<Node>(), null as Bounds?)){ (selShapes, selBounds), shape ->
-            val shapeBounds = shape.boundsInParent
-
-            if (selBounds == null) {
-                Pair(selShapes + Shape.selection(shape), shapeBounds)
-            } else {
-                Pair(selShapes + Shape.selection(shape), selBounds.union(shapeBounds))
-            }
-        }
-
-        children.setAll((selectionShapes + Shape.selectionBounds(selectionBounds!!, context)))
-
-        children.addAll(
-            selectionHandle(selectionBounds.minX, selectionBounds.minY, Cursor.NW_RESIZE),
-            selectionHandle(selectionBounds.maxX, selectionBounds.maxY, Cursor.SE_RESIZE),
-            selectionHandle(selectionBounds.maxX, selectionBounds.minY, Cursor.NE_RESIZE),
-            selectionHandle(selectionBounds.minX, selectionBounds.maxY, Cursor.SW_RESIZE),
-        )
-    }
-
-    fun selectionHandle(x: Double, y: Double, cursor: Cursor): Node {
-
+    init{
         val size = handleSize / context.scale
         val offset = size / 2
         val lineWidth = 1/ context.scale
 
-        return Rectangle(x-offset, y-offset, size, size).apply{
+        val rect = Rectangle(x-offset, y-offset, size, size).apply{
             fill = Color.WHITE
             stroke = Shape.selectionStroke
             strokeWidth = lineWidth
             this.cursor = cursor
         }
+        children.setAll(rect)
     }
 
-
 }
+
+
